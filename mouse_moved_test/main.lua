cb1646d33c7cf291cb3411c4ea481b01646871aa
xthomas <zitothomas@gmail.com>
2023-03-28 20:19:56 -1000

    testing mouse drag detection
    I will use drag mouse for painting the map

--- mouse_moved_test/main.lua
+++ mouse_moved_test/main.lua
@@ -5,12 +5,14 @@ function love.mousepressed(x, y, button)
     if button == 1 then -- left mouse button
         isDragging = true
         startX, startY = x, y
+        print("lmouse pressed")
     end
 end
 
 function love.mousereleased(x, y, button)
     if button == 1 then -- left mouse button
         isDragging = false
+        print("lmouse released")
     end
 end
 
@@ -18,21 +20,32 @@ function love.mousemoved(x, y, dx, dy)
     if isDragging then
         -- mouse is being dragged
         -- do something with dx and dy
+        print("mouse moved")
     end
 end
 
 function love.touchpressed(id, x, y)
     isDragging = true
     startX, startY = x, y
+    print("touch pressed")
+
 end
 
 function love.touchreleased(id, x, y)
     isDragging = false
+    print("touch released")
 end
 
 function love.touchmoved(id, x, y, dx, dy)
     if isDragging then
         -- touch is being dragged
         -- do something with dx and dy
+        print("touch moved")
     end
+end
+
+function love.conf(t)
+    t.console = true
+    t.modules.joystick = false
+    t.modules.physics = false
 end
